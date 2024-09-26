3fdd354662d47de28dccfb64244e71055428a6e2
drewfunderburk <kevinfunderburkbusiness@gmail.com>
2024-09-25 20:29:06 -0500

    Index buffer

--- TetrisClone/src/Rendering/Window.cpp
+++ TetrisClone/src/Rendering/Window.cpp
@@ -73,19 +73,31 @@ bool Window::init(const char* title, int width, int height)
 	float positions[] =
 	{
 		-0.5f, -0.5f,
-		 0.0f,  0.5f,
-		 0.5f, -0.5f
+		 0.5f, -0.5f,
+		 0.5f,  0.5f,
+		-0.5f,  0.5f
 	};
+
+	unsigned int indices[] =
+	{
+		0, 1, 2,
+		2, 3, 0
+	};
+
 	unsigned int buffer;
 	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
+
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
-	// Create GL shader
+	unsigned int ibo;
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
-
+	// Create GL Shader
 	unsigned int shader = Shader::LoadShader("src/Rendering/Basic.shader");
 	glUseProgram(shader);
 
@@ -100,7 +112,7 @@ void Window::update()
 		// Clear the buffer
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
 		// Swap the buffers
 		glfwSwapBuffers(m_window);
